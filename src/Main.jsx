e123648d1d8fe02eaee7b6254884aca52629fb8f
Dangchan Kim <kdc5847@naver.com>
2024-06-14 21:05:19 +0900

    feat: CreatedRollingListPage 변경

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import App from './App';
-import HomePage from './pages/HomePage/HomePage';
-import CreatedRollingListPage from './pages/CreatedRollingListPage/CreatedRollingListPage';
-import ListPage from './pages/ListPage/ListPage';
-import PostMessagePage from './pages/PostMessagePage/PostMessagePage';
-import CreateRollingPage from './pages/CreateRollingPage/CreateRollingPage';
-import EditPage from './pages/PostMessagePage/EditPage';
+import { Routes, Route } from "react-router-dom";
+import App from "./App";
+// import HomePage from './pages/HomePage/HomePage';
+import CreatedRollingListPage from "./pages/CreatedRollingListPage/CreatedRollingListPage";
+// import ListPage from './pages/ListPage/ListPage';
+// import PostMessagePage from './pages/PostMessagePage/PostMessagePage';
+// import CreateRollingPage from './pages/CreateRollingPage/CreateRollingPage';
+// import EditPage from './pages/PostMessagePage/EditPage';
 
 function Main() {
   return (
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path="/" element={<CreatedRollingListPage />} />
+
+      {/* <Route path="/" element={<App />}>
         <Route index element={<HomePage />} />
         <Route path="list" element={<ListPage />} />
         <Route path="post">
@@ -19,7 +21,7 @@ function Main() {
           <Route path=":id/edit" element={<EditPage />} />
           <Route path=":id/message" element={<PostMessagePage />} />
         </Route>
-      </Route>
+      </Route> */}
     </Routes>
   );
 }
--- src/pages/CreatedRollingListPage/CreatedRollingListPage.jsx
+++ src/pages/CreatedRollingListPage/CreatedRollingListPage.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react';
-import ListStyles from './CreatedRollingListPage.module.scss';
-import Modal from './Modal';
-import Card from './Card';
-import DeleteBtn from './DeleteBtn';
-import DeleteModal from './DeleteModal';
-import plusEnabledIcon from '../../assets/icons/Plusbtn_Enabled_ic.svg';
+import { useState } from "react";
+import ListStyles from "./CreatedRollingListPage.module.scss";
+import Modal from "./Modal";
+import Card from "./Card";
+import DeleteBtn from "./DeleteBtn";
+import DeleteModal from "./DeleteModal";
+import plusEnabledIcon from "../../assets/icons/plusbtn_enabled_ic.svg";
 
 const CreatedRollingListPage = () => {
   const [rollingList, setRollingList] = useState({});
   const [disabled, setDisabled] = useState(false);
   return (
-    <div className={ListStyles['background-container']}>
-      <div className={ListStyles['list-container']}>
-        <div className={ListStyles['flex-end']}>
+    <div className={ListStyles["background-container"]}>
+      <div className={ListStyles["list-container"]}>
+        <div className={ListStyles["flex-end"]}>
           <DeleteBtn />
         </div>
-        <div className={ListStyles['list-wrap']}>
-          <div className={ListStyles['create-container']}>
+        <div className={ListStyles["list-wrap"]}>
+          <div className={ListStyles["create-container"]}>
             <button
               type="button"
               disabled={disabled}
-              className={ListStyles['create-btn']}
+              className={ListStyles["create-btn"]}
             >
               <img src={plusEnabledIcon} alt="카드생성아이콘" />
             </button>

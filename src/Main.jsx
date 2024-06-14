3faa3b3dc413bf997dc853530c1c646b5c4c1599
Dangchan Kim <kdc5847@naver.com>
2024-06-14 20:21:34 +0900

    feat: Main.jsx 추가

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,24 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-import App from "./App";
-// import HomePage from "./pages/HomePage/HomePage";
-// import CreatedRollingListPage from "./pages/CreatedRollingListPage/CreatedRollingListPage";
-// import ListPage from "./pages/ListPage/ListPage";
-// import PostMessagePage from "./pages/PostMessagePage/PostMessagePage";
-// import CreateRollingPage from "./pages/CreateRollingPage/CreateRollingPage";
-// import EditPage from "./pages/PostMessagePage/EditPage";
+import { Routes, Route } from 'react-router-dom';
+import App from './App';
+import HomePage from './pages/HomePage/HomePage';
+import CreatedRollingListPage from './pages/CreatedRollingListPage/CreatedRollingListPage';
+import ListPage from './pages/ListPage/ListPage';
+import PostMessagePage from './pages/PostMessagePage/PostMessagePage';
+import CreateRollingPage from './pages/CreateRollingPage/CreateRollingPage';
+import EditPage from './pages/PostMessagePage/EditPage';
 
 function Main() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
-        {/* <Route index element={<HomePage />} /> */}
-        {/* <Route path="list" element={<ListPage />} />
+        <Route index element={<HomePage />} />
+        <Route path="list" element={<ListPage />} />
         <Route path="post">
           <Route index element={<CreateRollingPage />} />
           <Route path=":id" element={<CreatedRollingListPage />} />
           <Route path=":id/edit" element={<EditPage />} />
           <Route path=":id/message" element={<PostMessagePage />} />
-        </Route> */}
+        </Route>
       </Route>
     </Routes>
   );

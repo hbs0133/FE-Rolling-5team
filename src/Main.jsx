895f3202f6b607cd6ff7f0ac1077f7e26db28f24
김가영 <gy2039587@naver.com>
2024-06-15 13:45:25 +0900

    feat: 메인 페이지 구현

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import App from "./App";
-// import HomePage from "./pages/HomePage/HomePage";
+import HomePage from "../src/pages/HomePage/HomePage";
 // import CreatedRollingListPage from "./pages/CreatedRollingListPage/CreatedRollingListPage";
 // import ListPage from "./pages/ListPage/ListPage";
 // import PostMessagePage from "./pages/PostMessagePage/PostMessagePage";
@@ -11,7 +11,7 @@ function Main() {
   return (
     <Routes>
       <Route path="/" element={<App />}>
-        {/* <Route index element={<HomePage />} /> */}
+        <Route index element={<HomePage />} />
         {/* <Route path="list" element={<ListPage />} />
         <Route path="post">
           <Route index element={<CreateRollingPage />} />

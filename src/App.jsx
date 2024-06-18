c37f28525a49d6191506f2564f5179853aff28b9
[K777agent] <[junzang00@gmail.com]>
2024-06-18 15:49:56 +0900

    Feet: 리스트 페이지 완성본(mk_2)

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import '../src/styles/global.css'
 import '../src/styles/common.scss'
 import { Outlet } from 'react-router-dom'
 import Header from './components/layout/Header/Header'
-import ListPage from './pages/ListPage/ListPage'
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
-      <ListPage />
     </>
   )
 }

bb9e9f7f76dd323e1091cf05a50dd543c8ab2e10
Dangchan Kim <kdc5847@naver.com>
2024-06-18 16:26:28 +0900

    feat: Click이벤트 기능 추가

--- src/pages/CreatedRollingListPage/DeleteModal.jsx
+++ src/pages/CreatedRollingListPage/DeleteModal.jsx
@@ -1,22 +1,50 @@
-import DeleteModalStyles from './DeleteModal.module.scss';
+import DeleteModalStyles from "./DeleteModal.module.scss";
 
-const DeleteModal = () => {
+const DeleteModal = ({ name, title, setIsModal, setIsDeleteModal, key }) => {
+  const handleCheckBtnClick = () => {
+    setIsDeleteModal((prev) => ({
+      ...prev,
+      isModal: false,
+      modalId: 0,
+    }));
+
+    setIsModal((prev) => ({
+      ...prev,
+      modalId: 0,
+    }));
+  };
+
+  const handleCloseBtnClick = () => {
+    setIsDeleteModal((prev) => ({
+      ...prev,
+      isModal: false,
+      modalId: 0,
+    }));
+  };
   return (
-    <div className={DeleteModalStyles['background-opacity']}>
-      <div className={DeleteModalStyles['deleteModal-container']}>
-        <div className={DeleteModalStyles['deleteModal-checkMessage']}>
-          정말 삭제하시겠습니까?
+    <div className={DeleteModalStyles["background-opacity"]} key={key}>
+      <div className={DeleteModalStyles["deleteModal-container"]}>
+        <div className={DeleteModalStyles["deleteModal-checkMessage"]}>
+          <h2>
+            {name}님의 {title} 삭제
+          </h2>
+          <div>
+            삭제하면 복구할 수 없습니다. <br />
+            정말로 삭제하시겠습니까?
+          </div>
         </div>
-        <div className={DeleteModalStyles['button-wrap']}>
+        <div className={DeleteModalStyles["button-wrap"]}>
           <button
             type="button"
-            className={DeleteModalStyles['deleteModal-check-btn']}
+            className={DeleteModalStyles["deleteModal-check-btn"]}
+            onClick={handleCheckBtnClick}
           >
             확인
           </button>
           <button
             type="button"
-            className={DeleteModalStyles['deleteModal-close-btn']}
+            className={DeleteModalStyles["deleteModal-close-btn"]}
+            onClick={handleCloseBtnClick}
           >
             취소
           </button>

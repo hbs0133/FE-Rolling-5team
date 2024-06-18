25e47c0046bf94b545a57cdea280d09bea1f22ea
Dangchan Kim <kdc5847@naver.com>
2024-06-18 16:27:13 +0900

    feat: click이벤트 기능 추가

--- src/pages/CreatedRollingListPage/Card.jsx
+++ src/pages/CreatedRollingListPage/Card.jsx
@@ -1,44 +1,42 @@
 import { useState } from "react";
 import CardStyles from "./Card.module.scss";
 import Badge from "./Badge";
-import deleteIcon from "../../assets/icons/delete_ic.svg";
 
-const Card = () => {
+const Card = ({ recentMessages, key, setIsModal }) => {
+  const handleCardClick = () => {
+    setIsModal((prev) => ({
+      ...prev,
+      isModal: true,
+      modalId: recentMessages.id,
+    }));
+  };
+
   return (
     <>
-      <div className={CardStyles.container}>
+      <div className={CardStyles.container} key={key} onClick={handleCardClick}>
         <div className={CardStyles["profile-wrap"]}>
           <div>
             <img
-              src="#"
+              src={recentMessages.profileImageURL}
               alt="프로필이미지"
               className={CardStyles["profile-img"]}
             />
             <div>
               <h2 className={CardStyles["profile-name"]}>
-                From. <span>김당찬</span>
+                From. <span>{recentMessages.sender}</span>
               </h2>
-              <Badge>지인</Badge>
+              <Badge>{recentMessages.relationship}</Badge>
             </div>
           </div>
-          <button type="button" className={CardStyles["delete-btn"]}>
-            <img src={deleteIcon} alt="삭제버튼" />
-          </button>
         </div>
-        <p className={CardStyles.description}>
-          안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬 주세요안녕하십니까
-          고기반찬 주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬 주세요
-          안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬 주세요안녕하십니까
-          고기반찬 주세요안녕하십니까 고기반찬 주세요안녕하십니까 고기반찬
-          주세요
-        </p>
-        <div className={CardStyles.date}>2020.02.20</div>
+        <p
+          className={CardStyles.description}
+          dangerouslySetInnerHTML={{ __html: recentMessages.content }}
+          style={{ fontFamily: recentMessages.font }}
+        ></p>
+        <div className={CardStyles.date}>
+          {recentMessages.createdAt.replace(/-/g, ".").split("T")[0]}
+        </div>
       </div>
     </>
   );

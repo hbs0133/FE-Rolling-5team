4b3170f0bd55be7c921ad87f1e2bce6602ac9763
서미영 <seomiyoung3998@gmail.com>
2024-06-18 23:40:38 +0900

    usedevice 통합 및 일부 수정

--- src/components/UI/ReactionEmoji/TopReactions.jsx
+++ src/components/UI/ReactionEmoji/TopReactions.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import ReactionEmojiStyles from './ReactionEmoji.module.scss'
-import useRecipientData from '../../../hooks/useRecipientReactionData'
+import React from "react";
+import ReactionEmojiStyles from "./ReactionEmoji.module.scss";
+import useRecipientData from "../../../hooks/useRecipientReactionData";
 
 function TopReactions({
   id = 0,
   topReactions = [],
   onSelectedEmoji = () => {},
 }) {
-  const { recipientData } = useRecipientData(id)
+  const { recipientData } = useRecipientData(id);
   const reactions =
-    topReactions.length !== 0 ? topReactions : recipientData.topReactions
+    topReactions.length !== 0 ? topReactions : recipientData.topReactions;
   const emojis = {
     id: 0,
-    native: '',
-  }
+    native: "",
+  };
 
   const handleEmojiSelect = (id, emoji) => {
-    emojis.id = id
-    emojis.native = emoji
-    onSelectedEmoji(emojis)
-  }
+    emojis.id = id;
+    emojis.native = emoji;
+    onSelectedEmoji(emojis);
+  };
 
   return (
-    <div className={ReactionEmojiStyles['emoji-container']}>
+    <div className={ReactionEmojiStyles["emoji-container"]}>
       {reactions.map(({ id, emoji, count }) => (
         <span
           key={id}
-          className={ReactionEmojiStyles['emoji-button']}
+          className={ReactionEmojiStyles["emoji-button"]}
           onClick={() => handleEmojiSelect(id, emoji)}
         >
-          <spna className={ReactionEmojiStyles['emoji-both']}>
-            <span className={ReactionEmojiStyles['emoji']}>{emoji} </span>
-            <span className={ReactionEmojiStyles['emoji']}>{count}</span>
-          </spna>
+          <span className={ReactionEmojiStyles["emoji-both"]}>
+            <span className={ReactionEmojiStyles["emoji"]}>{emoji} </span>
+            <span className={ReactionEmojiStyles["emoji"]}>{count}</span>
+          </span>
         </span>
       ))}
     </div>
-  )
+  );
 }
 
-export default TopReactions
+export default TopReactions;
